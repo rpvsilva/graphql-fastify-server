10d6e2bde8770f4688fbef620122df3fd43e1d0c
Rui Silva <ruipedrosilva.1998@gmail.com>
2022-03-12 14:22:43 +0000

    chore: accept resolvers and typeDefs separated (#10)

--- src/types/server.ts
+++ src/types/server.ts
@@ -5,15 +5,24 @@ import { RenderPageOptions } from 'graphql-playground-html';
 import { ObjectOfAny } from '../types/misc';
 import { Cache } from './cache';
 import { Middlewares } from './middlewares';
+import { TypeSource } from '@graphql-tools/utils';
 
-export type GraphQLFastifyConfig<R = any> = {
-  schema: GraphQLSchema;
+export type GraphQLFastifyConfig<R = any> = SchemaConfig & {
   debug?: boolean;
   playground?: PlaygroundOptions;
   context?: (request: FastifyRequest) => ObjectOfAny;
   cache?: Cache<R>;
   middlewares?: Middlewares;
 };
+
+type SchemaConfig =
+  | {
+      schema: GraphQLSchema;
+    }
+  | {
+      typeDefs: TypeSource;
+      resolvers: ObjectOfAny;
+    };
 
 export type PlaygroundOptions = RenderPageOptions & {
   enabled?: boolean;
@@ -26,3 +35,6 @@ export type GraphQLBody = {
   operationName?: string;
   variables?: ObjectOfAny;
 };
+
+export const isSchemaType = (config: ObjectOfAny): config is { schema: GraphQLSchema } =>
+  !!config.schema;
